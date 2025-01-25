c4c6570f38bc52f5a0183b88e0d2a7065605edf6
pedrozz13755 <pedro13755@gmail.com>
2025-01-25 13:41:46 -0300

    Mensagem do commit

---
+++ laura.sh
@@ -0,0 +1,35 @@
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+PLTREMTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+PURPLE='\033[1;31m'
+PLTREMTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+PLTREMTRED='\033[1;34m'
+PLTREMTPURPLE='\033[1;35m'
+PLTREMTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+while : 
+do
+echo ""
+echo ""
+echo "${GREEN}╭╮╶╶╶╶╶╶╶╶╶╶╶╶╶╶╶╶╶╭─╮╶╶╶╭╮╶╶╶╶╶╶╶╶╶╭╮╶ 
+││╶╭─╮╶╭┬╮╭┬╮╭─╮╶╶╶│││╭┬╮├┤╭─┬─╮╭─╮╶│╰╮ 
+│╰╮││╰╮││││╭╯││╰╮╶╶│╭╯│╭╯││╰╮│╭╯││╰╮│╭┤ 
+╰─╯╰──╯╰─╯╰╯╶╰──╯╶╶╰╯╶╰╯╶╰╯╶╰─╯╶╰──╯╰─╯ 
+"
+echo ""
+echo "${GREEN}PEDROZZ MODS SÓ MANDANDO BOT DE QUALIDADE 😎"
+echo "${RED}YOUTUBE: PEDROZZ_MODS"
+echo "${ORANGE}INSTAGRAM: PEDROZZ_13755"
+echo "${RED}OBS: A conexão por código foi tirado por conta de erros da baileys..."
+echo "${RED}〔 AUTO CONEXÃO PARA PREVENÇÃO DE QUEDAS 〕"
+echo "${NOCOLOR}"
+
+node conexão.js
+done
